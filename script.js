8796d473998d6f71666699b88b74bd537b47db42
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-09-30 20:07:16 +0700

    made it mobile-friendly

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let taskList = document.getElementById('taskList');
-let newTask = document.getElementById('newTask');
-let taskListHeader = document.getElementById('taskListHeader');
+const taskList = document.getElementById('taskList');
+const taskCreator__editor = document.getElementById('taskCreator__editor');
+const taskListHeader = document.getElementById('taskListHeader');
+const save = document.getElementById('save');
+
 class Task {
-	constructor (text, made = false) {
+	constructor (text, done = false) {
 		this._task = document.createElement('li');
 		this._task.className = 'task';
 		taskList.append(this._task);
 	
-		this._done = document.createElement('input');
-		this._done.className = 'done';
-		this._done.type = 'checkbox';
-		this._task.append(this._done);
+		this._doneCheckbox = document.createElement('input');
+		this._doneCheckbox.className = 'doneCheckbox';
+		this._doneCheckbox.type = 'checkbox';
+		this._task.append(this._doneCheckbox);
 		
 		this._close = document.createElement('button');
 		this._close.className = 'close hidden';
@@ -23,22 +25,22 @@ class Task {
 		this._textOfTask.innerHTML = text;
 		this._task.append(this._textOfTask);
 	
-		this._made = made;
+		this._done = done;
 		this._running =  true;
 		this.run();
 	}
 
 	run() {
-		if(this._made) {
-			this._task.classList.add('made');
-			this._done.setAttribute('checked', 'checked');
+		if(this._done) {
+			this._task.classList.add('doneTask');
+			this._doneCheckbox.setAttribute('checked', 'checked');
 			this._close.classList.toggle('hidden');
 		}
 
-		this._done.onclick = () => {
-			this._task.classList.toggle('made');
+		this._doneCheckbox.onclick = () => {
+			this._task.classList.toggle('doneTask');
 			this._close.classList.toggle('hidden');
-			this._made ? this._made = false : this._made = true;
+			this._done ? this._done = false : this._done = true;
 			showCount();
 		};
 
@@ -48,28 +50,43 @@ class Task {
 			tasks = tasks.filter( task => {
 				return task._running;
 			});
+			taskCreator__editor.focus();
 		};
 
-		this._task.ondblclick = (e) => {
-			if(e.target.classList.contains('done')) return;
-			let edit = document.createElement('textarea');
+		this._task.ondblclick = () => {
+			if(event.currentTarget.classList.contains('doneTask')) return;
+			taskCreator__editor.value = '';
+			const edit = document.createElement('textarea');
 			edit.className = 'edit';
 			edit.style.height = `${this._task.offsetHeight}px`;
 			this._task.classList.toggle('hidden');
 			this._task.after(edit);
 			edit.value = this._textOfTask.innerHTML; // set focus to the last character in textarea
 			edit.focus();
+			
 			edit.onkeyup = (e) => {
 				if(e.key == 'Escape') {
-					edit.remove();
-					this._task.classList.toggle('hidden');
+					edit.blur();
 				}
 				if(e.key == 'Enter' && e.ctrlKey) {
 					this._textOfTask.innerHTML = edit.value;
-					edit.remove();
-					this._task.classList.toggle('hidden');
-				}		
+					edit.blur();
+				}
 			};
+			edit.onblur = () => {
+				save.addEventListener('click', editTask.bind(this));
+				edit.remove();
+				this._task.classList.toggle('hidden');
+				
+				// it's need to remove event listener ANYWAY,
+				// so we need to call removeEventListener 
+				// instead of call addEventListener with option once == true
+				// (option "once" wouldn't work if button wouldn't pressed)
+				save.removeEventListener('click', editTask.bind(this));
+			};
+			function editTask () {
+					this._textOfTask.innerHTML = edit.value;
+			}
 		};
 	}
 }
@@ -79,7 +96,7 @@ let tasks = [];
 
 // do on load
 document.addEventListener('DOMContentLoaded', () => {
-	newTask.focus();
+	taskCreator__editor.focus();
 	let tasksNumber = +localStorage['tasksNumber'];
 	if (!tasksNumber) return;
 	for(let i = 0; i < tasksNumber; i++) {
@@ -92,7 +109,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // do before unload
 window.onbeforeunload = () => {
 	tasks.forEach( (task, i) => {
-		let json = JSON.stringify( [ task._textOfTask.innerHTML, task._made] )
+		let json = JSON.stringify( [ task._textOfTask.innerHTML, task._done] )
 		localStorage[`task${i}`] = json;
 	});
 
@@ -107,24 +124,36 @@ window.onbeforeunload = () => {
 };
 
 // create a task
-newTask.onkeyup = function(e) {
-	if( !(e.key == 'Enter' && e.ctrlKey) || !this.value ) return;
-	tasks.push( new Task(this.value) );
-	showCount();
-	this.value = '';
+save.onclick = () => {
+	if(!taskCreator__editor.value) return;
+	addTask();
+	taskCreator__editor.focus();
+};
+taskCreator__editor.onkeyup = function(e) {
+	if(!(e.key == 'Enter' && e.ctrlKey) || !this.value) return;
+	addTask();
 };
 
+function addTask() {
+	tasks.push( new Task(taskCreator__editor.value) );
+	showCount();
+	taskCreator__editor.value = '';
+}
 function showCount() {
-	tasksToDo = tasks.filter( task => { return !task._made; } ).length;
+	tasksToDo = tasks.filter( task => { return !task._done; } ).length;
+	const tooltip = document.getElementById('tooltip');
 	switch (tasksToDo) {
 		case 0:
 			taskListHeader.innerHTML = `Nothing to do... boring...`;
+			tooltip.classList.add('hidden');
 			break;
 		case 1:
-			taskListHeader.innerHTML = `To do <span class="tasksToDo">1</span>  task <br> Double click to edit.<br>  "Ctrl+Enter" to save. Esc to exit`;
+			taskListHeader.innerHTML = `To do <span class="tasksToDo">1</span>  task`;
+			tooltip.classList.remove('hidden');
 			break;
 		default:
-			taskListHeader.innerHTML = `To do <span class="tasksToDo">${tasksToDo}</span> tasks <br> Double click to edit.<br>  "Ctrl+Enter" to save. Esc to exit`;
+			taskListHeader.innerHTML = `To do <span class="tasksToDo">${tasksToDo}</span> tasks`;
+			tooltip.classList.remove('hidden');
 			break;
 	}
 }
